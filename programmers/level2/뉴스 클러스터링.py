170e6642113f96d62ac15631aa0438cbcb259a9d
thkthk97@naver.com <thkthk97@naver.com>
2021-09-17 02:00:33 +0900

    2021-09-17

--- programmers/level2/뉴스 클러스터링.py
+++ programmers/level2/뉴스 클러스터링.py
@@ -1,31 +1,11 @@
-import collections
-import math
+import collections, math
+
 def solution(str1, str2):
-    def get_jacquard(arr1, arr2):
-        c1 = collections.Counter(arr1)
-        c2 = collections.Counter(arr2)
-        c3 = c1 - c2 + c2 # Counter객체는 음수인 것에 대해 자동으로 지운다. 따라서 A 합집합 B를 구할 수 있다.
-        total = sum(c3.values())
-        inter = set(arr1) & set(arr2)
-        inter_cnt = 0
-        for word in inter:
-            inter_cnt += min(arr1.count(word), arr2.count(word)) #교집합은 다중집합에서 공통부분의 개수중 작은 것을 더한다
-        return inter_cnt / total
-        
-        
-    def fill_arr(string):
-        arr = []
-        cnt = 0
-        idx = 0
-        temp_str = ""
-        while idx<len(string)-1:
-            if string[idx].isalpha() and string[idx+1].isalpha():
-                arr.append(string[idx:idx+2].lower())
-            idx += 1
-        return arr
-                
-    arr1 = fill_arr(str1)
-    arr2 = fill_arr(str2)
-    if not arr1 and not arr2: return 65536
-    else:
-        return math.floor(get_jacquard(arr1, arr2) * 65536)
+    str1, str2, counter = str1.lower(), str2.lower(), collections.Counter
+    multi_list1 = [str1[i:i+2] for i in range(len(str1)-1) if str1[i].isalpha() and str1[i+1].isalpha()]
+    multi_list2 = [str2[i:i+2] for i in range(len(str2)-1) if str2[i].isalpha() and str2[i+1].isalpha()]
+    if not multi_list1 and not multi_list2: return 65536
+    c1, c2 = counter(multi_list1), counter(multi_list2)
+    c1_and_c2 = counter(dict(map(lambda x: (x[0], x[1]//2), ((c1+c2) - ((c1-c2) + (c2-c1))).items())))
+    c1_or_c2 = (c1+c2) - (c1_and_c2)
+    return math.floor(sum(c1_and_c2.values()) / sum(c1_or_c2.values()) * 65536)
